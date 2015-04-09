d62374b298046b755ca3f063441d60a0c9ea5cc7
Antonio Hernández <antonio.hernandez@panamedia.net>
2015-04-09 17:21:55 +0200

    Php updated to 5.6 Phalcon updated to 2.0rc1

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,15 +1,30 @@
 #!/usr/bin/env bash
+####
+## INSTALLING PHP5-CLI
+## Installing php5-cli on the vagrant machine for PhpStorm configurations
+####
+sudo apt-get install -y software-properties-common
+sudo apt-add-repository -y ppa:ondrej/php5-5.6
+sudo apt-get clean
+sudo apt-get update -q
+sudo apt-get install -y php5-cli php5-xdebug
+
+####
+## CHECKING DOCKER INSTALLATION
+####
 dockerv=$(docker -v 2>&1)
 if [[ $dockerv != "Docker version 1.5.0"* ]]
 then
-  echo "NO TENEMOS DOCKER, INSTALANDO";
+  echo "DOCKER NOT FOUND, INSTALLING...";
   wget -qO- https://get.docker.com/ | sh
 fi
 docker login --email="antonio.hernandez@panamedia.net" --username="panamedia" --password="2Fsz8EGmy6LhKCR5"
 
-#build dev-www
+####
+## BUILDING DEVEL-WEB CONTAINER
+####
 cd /docker/devel-web
 sudo docker build -t="panamedia/devel-web" .
 sudo docker stop devel-web
 sudo docker rm devel-web
-sudo docker run -d -p 8080:80 --name devel-web panamedia/devel-web
+sudo docker run -v /src:/var/www/ -d -p 8080:80 --name devel-web panamedia/devel-web
