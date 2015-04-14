5c58446d4affc6a47c59a91ae8aa4b845761ab54
Antonio Hernández <antonio.hernandez@panamedia.net>
2015-04-14 18:09:54 +0200

    PhpUnit configurado para phalcon

--- install-dev-environment.sh
+++ install-dev-environment.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 git clone https://github.com/PanamediaSLU/euromillions.git src
+chmod 777 src/app/assets
+chmod 777 src/app/cache
 mkdir .git/hooks
 cp githooks/* .git/hooks
 cd vagrant
