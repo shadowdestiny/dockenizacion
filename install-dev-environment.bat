dcf59d789384057083466b3e36d8522178be0eae
Antonio Hernández <antonio.hernandez@panamedia.net>
2015-04-16 15:51:31 +0200

    Including src folder in the repository to make easier the handling of the project.
    The repository with the only the sourcecode it's obsolete from now on

--- install-dev-environment.bat
+++ install-dev-environment.bat
@@ -1,4 +1,3 @@
-git clone https://github.com/PanamediaSLU/euromillions.git src
 mkdir .git/hooks
 cp githooks/* .git/hooks
 cd vagrant
--- install-dev-environment.sh
+++ install-dev-environment.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-git clone https://github.com/PanamediaSLU/euromillions.git src
 chmod 777 src/app/assets
 chmod 777 src/app/cache
 mkdir .git/hooks
