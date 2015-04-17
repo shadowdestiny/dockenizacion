9fd80b88bab5b4985b717d65fa45830c6f84997f
Antonio Hernández <antonio.hernandez@panamedia.net>
2015-04-17 11:03:50 +0200

    Inserting some usage examples in the DoctrineTask

--- src/app/tasks/DoctrineTask.php
+++ src/app/tasks/DoctrineTask.php
@@ -1,12 +1,44 @@
 <?php
 namespace app\tasks;
+use app\entities\BillingProvider;
+use Doctrine\ORM\EntityManager;
 use Phalcon\CLI\Task;
 
 class DoctrineTask extends Task
 {
+    /** @var  EntityManager */
+    protected $entityManager;
+
+    public function initialize()
+    {
+        $this->entityManager = $this->getDI()->get('entityManager');
+    }
+
     public function getEntityManagerAction()
     {
         global $entityManager;
         $entityManager = $this->getDI()->get('entityManager');
+    }
+
+    public function InsertTestAction($params)
+    {
+        $billing_provider = new BillingProvider();
+        $billing_provider->setName($params[0]);
+
+        /** @var $em EntityManager */
+        $em = $this->getDI()->get('entityManager');
+        $em->persist($billing_provider);
+        $em->flush();
+
+        echo "created billing provider with ID ".$billing_provider->getId()."\n";
+    }
+
+    public function ListTestAction()
+    {
+        $billing_provider_repository = $this->entityManager->getRepository('\app\entities\BillingProvider');
+        $billing_providers = $billing_provider_repository->findAll();
+        foreach ($billing_providers as $bp) {
+            echo sprintf("-%s\n", $bp->getName());
+        }
     }
 }
