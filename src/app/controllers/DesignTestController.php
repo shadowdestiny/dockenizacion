3123469ebe3d9e5165126dbbab289fce575c61f6
Antonio Hernández <antonio.hernandez@panamedia.net>
2015-04-17 16:55:11 +0200

    View visualization system for the designers

--- src/app/controllers/DesignTestController.php
+++ src/app/controllers/DesignTestController.php
@@ -8,7 +8,18 @@ class DesignTestController extends ControllerBase
 {
     public function viewAction($controller, $view)
     {
-        $variables_file = APP_PATH . 'templateHelpers/' . $controller . '/' . $view . '.ini';
+        $view_variables_file = APP_PATH . 'templateHelpers/' . $controller . '/' . $view . '.ini';
+        $layout_variables_file = APP_PATH . 'templateHelpers/general.ini';
+        $this->setVarsFromIni($view_variables_file);
+        $this->setVarsFromIni($layout_variables_file);
+        $this->view->pick("$controller/$view");
+    }
+
+    /**
+     * @param $variables_file
+     */
+    protected function setVarsFromIni($variables_file)
+    {
         $values = new Ini($variables_file);
         foreach ($values->scalars as $name => $value) {
             $this->view->setVar($name, $value);
@@ -19,6 +30,5 @@ class DesignTestController extends ControllerBase
         foreach ($values->objects as $name => $value) {
             $this->view->setVar($name, json_decode($value));
         }
-        $this->view->pick("$controller/$view");
     }
 }
--- src/app/views/test/testView.volt
+++ src/app/views/test/testView.volt
@@ -2,9 +2,11 @@
 variable 1 (string) <strong>{{ variable1 }}</strong><br>
 variable 2 (number) <strong>{{ variable2 }}</strong><br>
 variable 3 (array of one string and one integer)
-<strong>{% for product in variable3 %}
+<strong>
+    {% for product in variable3 %}
 {{ product }},
-{% endfor %}</strong>
+{% endfor %}
+</strong>
 <br>
 variable 4 (array)
 <strong>{{ variable4['elem1'] }} - {{ variable4['elem2'] }}</strong>
