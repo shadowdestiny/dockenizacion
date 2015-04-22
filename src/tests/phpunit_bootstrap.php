4914b9588997f07fe70b751a4c2272b961e43876
Antonio Hernández <antonio.hernandez@panamedia.net>
2015-04-22 12:48:32 +0200

    Configuring circle ci

--- src/tests/phpunit_bootstrap.php
+++ src/tests/phpunit_bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 define('TESTS_PATH', __DIR__);
 define('APP_PATH', TESTS_PATH . '/../app/');
-require_once APP_PATH.'vendor/autoload.php';
+require_once APP_PATH.'../vendor/autoload.php';
 require_once(APP_PATH . 'config/bootstrap/WebLoader.php');
 $loader = new \app\config\bootstrap\WebLoader(APP_PATH, TESTS_PATH.'/');
 $loader->register();
