9f2cbee000009cde1dcec086e0ef78ffdbf89cac
Antonio Hernández <antonio.hernandez@panamedia.net>
2015-04-23 11:51:42 +0200

    Adding environment variable to separate execution on shippable and local

--- src/tests/base/TestListener.php
+++ src/tests/base/TestListener.php
@@ -10,15 +10,21 @@ class TestListener extends \PHPUnit_Framework_BaseTestListener
 {
     public function startTestSuite(PHPUnit_Framework_TestSuite $suite)
     {
+        if (isset($_ENV['TEST_ENV']) && $_ENV['TEST_ENV'] == 'shippable') {
+            $config_file = 'shippableconfig.ini';
+        } else {
+            $config_file = 'testconfig.ini';
+        }
+
         if ($suite->getName() == "integration") {
             $config = DI::getDefault()->get('config');
             $command = "mysql -h {$config->database->host} -u {$config->database->username} -p{$config->database->password} -e 'CREATE DATABASE IF NOT EXISTS {$config->database->dbname};' 2>/dev/null";
             exec($command);
             $command = "mysqldump -h {$config->database->host} -u {$config->database->username} -p{$config->database->password} -d {$config->database->original_db_name} 2>/dev/null | mysql -h {$config->database->host} -u {$config->database->username} -p{$config->database->password} -D{$config->database->dbname} 2>/dev/null";
             exec($command);
-            $bootstrap = new Bootstrap(new TestWebBootstrapStrategy(false, APP_PATH,APP_PATH . 'config/'  , APP_PATH . 'assets/', TESTS_PATH . '/../', 'testconfig.ini'));
+            $bootstrap = new Bootstrap(new TestWebBootstrapStrategy(false, APP_PATH,APP_PATH . 'config/'  , APP_PATH . 'assets/', TESTS_PATH . '/../', $config_file));
         } else {
-            $bootstrap = new Bootstrap(new TestWebBootstrapStrategy(true, APP_PATH, APP_PATH . 'config/' , APP_PATH . 'assets/', TESTS_PATH . '/../', 'testconfig.ini'));
+            $bootstrap = new Bootstrap(new TestWebBootstrapStrategy(true, APP_PATH, APP_PATH . 'config/' , APP_PATH . 'assets/', TESTS_PATH . '/../', $config_file));
         }
         $bootstrap->execute();
     }
