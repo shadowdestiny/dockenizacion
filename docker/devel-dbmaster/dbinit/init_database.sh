92debd5ae243c39b6b57b8206054c08f53d98280
Antonio Hernández <antonio.hernandez@panamedia.net>
2015-05-04 12:29:58 +0200

    Fix the mysql container to run properly with docker-compose

--- docker/devel-dbmaster/dbinit/init_database.sh
+++ docker/devel-dbmaster/dbinit/init_database.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-/docker-entrypoint.sh
-sleep 20
+sleep 30
 mysql -u root -ptpl9 euromillions < /dbinit/init_structure.sql
 cat /dbinit/data/*.sql | mysql -u root -ptpl9 euromillions --default-character-set=utf8
