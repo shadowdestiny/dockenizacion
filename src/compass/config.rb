00f788bf2fb5af58b2787270123029c7712ba609
Antonio Hernández <antonio.hernandez@panamedia.net>
2015-05-04 18:05:50 +0200

    Make compass work

--- src/compass/config.rb
+++ src/compass/config.rb
@@ -1,10 +1,9 @@
 # Require any additional compass plugins here.
 
 #Folder settings
-project_path = "/src"
-css_dir = "public/css"          #where the CSS will saved
-sass_dir = "compass/sass"           #where our .scss files are
-images_dir = "public/img"    #the folder with your images
+css_dir = "../public/css"          #where the CSS will saved
+sass_dir = "sass"           #where our .scss files are
+images_dir = "../public/img"    #the folder with your images
 
 # You can select your preferred output style here (can be overridden via the command line):
 output_style = :expanded # After dev :compressed
