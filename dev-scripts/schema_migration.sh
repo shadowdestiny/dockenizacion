dac0e00779f354af938d23fbc787fc1bbb0c8c98
Antonio Hernández <antonio.hernandez@panamedia.net>
2015-05-14 17:38:27 +0200

    Regeneratig migrations

--- dev-scripts/schema_migration.sh
+++ dev-scripts/schema_migration.sh
@@ -2,18 +2,16 @@
 environment="$1"
 echo "executing schema migration on $1"
 if [ "$environment" == "dev" ]; then
-    echo "1"
     cd /var/www
     now=$(date)
     echo "$now: Schema migration executed" >> /vagrant/dev-logs/migration.log
     phalcon migration run
 elif [ "$environment" == "shippable" ]; then
     echo "migrating..."
-    cd src/
+    cd src/pjh
     vendor/bin/phalcon.php migration run
 fi
 if [ "$environment" == "shippable" ]; then
-    echo "2"
     cd ..
 fi
 echo "finish migration"
