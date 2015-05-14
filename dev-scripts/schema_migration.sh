9f48c77fcb21d8ed5d4b17161a5d537f44cc057d
Antonio Hernández <antonio.hernandez@panamedia.net>
2015-05-14 16:41:49 +0200

    Debugging migrations in shippable

--- dev-scripts/schema_migration.sh
+++ dev-scripts/schema_migration.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 environment="$1"
 echo "executing schema migration on $1"
-if [ environment = "dev" ]; then
+if [ environment == "dev" ]; then
     cd /var/www
     now=$(date)
     echo "$now: Schema migration executed" >> /vagrant/dev-logs/migration.log
     phalcon migration run
-elif [ environment = "shippable" ]; then
+elif [ environment == "shippable" ]; then
     echo "migrating..."
     cd src/
     vendor/bin/phalcon.php migration run
