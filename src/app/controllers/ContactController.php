5099ad5115dc87ef40e6696e8a646f731d29f119
Antonio Hernández <antonio.hernandez@panamedia.net>
2015-08-31 11:35:08 +0200

    Started work on contact Form so Raul can finish it

--- src/app/controllers/ContactController.php
+++ src/app/controllers/ContactController.php
@@ -1,12 +1,14 @@
 <?php
 namespace EuroMillions\controllers;
 
+use EuroMillions\forms\GuestContactForm;
+
 class ContactController extends PublicSiteControllerBase
 {
     public function guestAction()
     {
         $errors = null;
-        $contact_form = new ContactForm();
+        $contact_form = new GuestContactForm();
 
         //EMTD to be completed by Raul (example to follow: UserAccess/signUp )
         //Tip: create a value object ContactFormInfo
