8623570bdf5724349da74b2957fae829341a8dfe
rmesa <rmrbest@gmail.com>
2015-09-04 11:19:37 +0200

    add validation_token column

---
+++ src/app/migrations/20150904091856_add_validation_token.php
@@ -0,0 +1,19 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AddValidationToken extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     */
+    public function change()
+    {
+        $this->execute("ALTER TABLE users ADD validation_token VARCHAR(255) DEFAULT NULL;");
+    }
+}
