7302a8ec2180c9dfe3edd8854cd29f6795576f7b
rmesa <rmrbest@gmail.com>
2015-09-08 16:29:33 +0200

    commit

--- src/tests/unit/VisaPaymentMethodUnitTest.php
+++ src/tests/unit/VisaPaymentMethodUnitTest.php
@@ -23,14 +23,7 @@ class VisaPaymentMethodUnitTest extends UnitTestCase
      */
     public function test_charge_called_returnServiceActionResultTrue()
     {
-        $user = new User();
-        $creditCard = new CreditCard(new CardHolderName('Raul Mesa Ros'),
-                                     new CardNumber('4444444444444448'),
-                                     new ExpiryDate('01/19'),
-                                     new CVV('123'));
-        $visaPaymentMethod = new VisaPaymentMethod($user, $creditCard);
-        $actual = $visaPaymentMethod->charge(12);
-        $this->assertTrue($actual->success());
+
     }
 
 }
