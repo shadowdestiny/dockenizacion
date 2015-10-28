b8638444795c675e3197afa78c7a34c16397992c
rmesa <rmrbest@gmail.com>
2015-10-28 12:36:55 +0100

    fix path app

--- src/tests/phpunit_bootstrap.php
+++ src/tests/phpunit_bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 $test_path = __DIR__;
 $app_path = $test_path . '/../apps';
-require_once($app_path . '../vendor/autoload.php');
+require_once($app_path . '/../vendor/autoload.php');
 require_once($app_path . 'shareconfig/bootstrap/WebLoader.php');
 
 $loader = new \EuroMillions\shareconfig\bootstrap\WebLoader($app_path, $test_path);
