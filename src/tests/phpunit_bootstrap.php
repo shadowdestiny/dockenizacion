aadffe0608ec3c2bfd5083e3f97c6cd03bf7d928
rmesa <rmrbest@gmail.com>
2015-10-28 14:18:47 +0100

    fix path in vendor and shareconfig

--- src/tests/phpunit_bootstrap.php
+++ src/tests/phpunit_bootstrap.php
@@ -1,7 +1,8 @@
 <?php
 define('TESTS_PATH', __DIR__.'/');
-define('APP_PATH', TESTS_PATH . '../apps/');
-require_once APP_PATH.'../vendor/autoload.php';
-require_once(APP_PATH . 'shareconfig/bootstrap/WebLoader.php');
+define('APP_PATH', TESTS_PATH.'../apps/');
+require_once TESTS_PATH.'../vendor/autoload.php';
+require_once(TESTS_PATH.'../apps/shareconfig/bootstrap/WebLoader.php');
+
 $loader = new \EuroMillions\shareconfig\bootstrap\WebLoader(APP_PATH, TESTS_PATH);
 $loader->register();
