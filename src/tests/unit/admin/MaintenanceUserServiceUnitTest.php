1a1a303f4f6733808906e547890173a65734443e
rmesa <rmrbest@gmail.com>
2015-10-29 17:54:30 +0100

    update user balance unit test

--- src/tests/unit/admin/MaintenanceUserServiceUnitTest.php
+++ src/tests/unit/admin/MaintenanceUserServiceUnitTest.php
@@ -61,6 +61,44 @@ class MaintenanceUserServiceUnitTest extends UnitTestBase
         $this->assertEquals($expected,$actual);
     }
 
+    /**
+     * method updateUserBalance
+     * when calledWithValidUserId
+     * should updateBalanceFromUser
+     */
+    public function test_updateUserBalance_calledWithValidUserId_updateBalanceFromUser()
+    {
+        $user = $this->getUsers();
+        $amount = new Money(5000, new Currency('EUR'));
+        $this->userRepository_double->find($user->getId())->willReturn($user);
+        $this->userRepository_double->add($user);
+        $entityManager_stub = $this->getEntityManagerDouble();
+        $entityManager_stub->flush()->shouldNotBeCalled();
+        $this->stubEntityManager($entityManager_stub);
+        $sut = $this->getSut();
+        $actual = $sut->updateBalance(new UserId('9098299B-14AC-4124-8DB0-19571EDABE55'), $amount);
+        $this->assertTrue($actual->success());
+    }
+
+    /**
+     * method updateUserBalance
+     * when calledWithInvalidUserId
+     * should returnActionResultFalse
+     */
+    public function test_updateUserBalance_calledWithInvalidUserId_returnActionResultFalse()
+    {
+        $user = $this->getUsers();
+        $amount = new Money(5000, new Currency('EUR'));
+        $this->userRepository_double->find($user->getId())->willReturn(null);
+        $this->userRepository_double->add($user);
+        $entityManager_stub = $this->getEntityManagerDouble();
+        $entityManager_stub->flush()->shouldNotBeCalled();
+        $this->stubEntityManager($entityManager_stub);
+        $sut = $this->getSut();
+        $actual = $sut->updateBalance(new UserId('9098299B-14AC-4124-8DB0-19571EDABE55'), $amount);
+        $this->assertFalse($actual->success());
+    }
+
     private function getSut()
     {
         return $sut = $this->getDomainAdminServiceFactory()->getMaintenanceService();
@@ -83,16 +121,6 @@ class MaintenanceUserServiceUnitTest extends UnitTestBase
                 'validated' => false,
                 'balance' => new Money(5000,new Currency($currency)),
                 'validation_token' => '33e4e6a08f82abb38566fc3bb8e8ef0d'
-            ],
-            [
-                'id' => new UserId('9098299B-14AC-4124-8DB0-19571EDABE556'),
-                'name'     => 'test',
-                'surname'  => 'test02',
-                'email'    => new Email('raul.mesa2@panamedia.net'),
-                'password' => new Password('passworD01', new NullPasswordHasher()),
-                'validated' => false,
-                'balance' => new Money(5000,new Currency($currency)),
-                'validation_token' => '33e4e6a08f82abb38566fc3bb8e8ef0e'
             ]
         );
         return $user;
