c0c0c601341fe685a9668a95291b06d79e2b9c2d
Antonio Hernández <antonio.hernandez@panamedia.net>
2015-11-27 10:45:20 +0100

    New tests for EmSelect

--- src/react/components/__tests__/EmSelect-test.js
+++ src/react/components/__tests__/EmSelect-test.js
@@ -15,22 +15,22 @@ describe('EuroMillions Select', function() {
 
     it('renders provided options', function () {
 
-       var em_select = TestUtils.renderIntoDocument(
+       let em_select = TestUtils.renderIntoDocument(
            <EmSelect options={options} />
        );
-       var actual_options = ReactDOM.findDOMNode(em_select).childNodes[1].childNodes;
+       let actual_options = ReactDOM.findDOMNode(em_select).childNodes[1].childNodes;
 
-       for(var i=0; i<4; i++) {
+       for(let i=0; i<4; i++) {
            expect(actual_options[i].text).toEqual(options[i].text);
            expect(actual_options[i].value).toEqual(options[i].value);
        }
     });
 
     it('div text corresponds to selected option', function() {
-        var em_select = TestUtils.renderIntoDocument(
+        let em_select = TestUtils.renderIntoDocument(
             <EmSelect options={options} defaultValue="75" defaultText="75 text" />
         );
-        var select = ReactDOM.findDOMNode(em_select).childNodes[1];
+        let select = ReactDOM.findDOMNode(em_select).childNodes[1];
         TestUtils.Simulate.change(select, {
             "target": {
                 "value": '100',
@@ -39,19 +39,19 @@ describe('EuroMillions Select', function() {
                 ]}
         });
 
-        var div_text = ReactDOM.findDOMNode(em_select).childNodes[0].textContent;
+        let div_text = ReactDOM.findDOMNode(em_select).childNodes[0].textContent;
 
         expect(div_text).toBe('100 text');
     });
 
     it('doesn\'t change the value nor the div text when the parent handleChange returns false', function (){
-        var parentOnChange = function(event) {
+        let parentOnChange = function(event) {
             return false;
         };
-        var em_select = TestUtils.renderIntoDocument(
+        let em_select = TestUtils.renderIntoDocument(
             <EmSelect options={options} defaultValue="75" defaultText="75 text" onChange={parentOnChange}/>
         );
-        var select = ReactDOM.findDOMNode(em_select).childNodes[1];
+        let select = ReactDOM.findDOMNode(em_select).childNodes[1];
         TestUtils.Simulate.change(select, {
             "target": {
                 "value": '100',
@@ -60,7 +60,7 @@ describe('EuroMillions Select', function() {
                 ]}
         });
 
-        var div_text = ReactDOM.findDOMNode(em_select).childNodes[0].textContent;
+        let div_text = ReactDOM.findDOMNode(em_select).childNodes[0].textContent;
 
         expect(div_text).toBe('75 text');
         expect(select.value).toBe('75');
