684b414faf8f5a478811f31d101760a2f1a76b38
Antonio Hernández <antonio.hernandez@panamedia.net>
2015-12-14 15:53:40 +0100

    Created a new test for EmCustomizableSelect
    Added post-merge management of docker modifications
    
    Added http2 apache

--- src/react/tests/components/EmCustomizableSelect.spec.js
+++ src/react/tests/components/EmCustomizableSelect.spec.js
@@ -10,15 +10,30 @@ import EmCustomizableSelect from '../../components/EmCustomizableSelect.jsx';
 import EmSelect from '../../components/EmSelect.jsx';
 
 describe('EmCustomizableSelect', function () {
+    var options = [
+        {text: '50 text', value: '50'},
+        {text: '75 text', value: '75'},
+        {text: '100 text', value: '100'},
+        {text: 'custom text', value: 'custom'}
+    ];
+
     it('renders an EmSelect element on startup', function () {
-        var options = [
-            {text: '50 text', value: '50'},
-            {text: '75 text', value: '75'},
-            {text: '100 text', value: '100'},
-            {text: 'custom text', value: 'custom'}
-        ];
-        var em_customizable_select = TestUtils.renderIntoDocument(<EmCustomizableSelect options={options} />);
+        var em_customizable_select = TestUtils.renderIntoDocument(<EmCustomizableSelect options={options}/>);
         var select = TestUtils.findRenderedComponentWithType(em_customizable_select, EmSelect);
         expect(select).to.not.equal(null);
     });
+
+    it('hides EmSelect on selection custom value', function () {
+        var em_customizable_select = TestUtils.renderIntoDocument(<EmCustomizableSelect options={options}/>);
+        em_customizable_select.handleChange({
+            "target": {
+                "value": 'custom',
+                "selectedOptions": [
+                    {"text": 'custom text'}
+                ]
+            }
+        });
+        var select = TestUtils.findRenderedComponentWithType(em_customizable_select, EmSelect);
+        expect(select).to.equal(null);
+    })
 });
