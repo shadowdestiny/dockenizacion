fc18d8416b65673e8f1fc96ecdf8eb7fdd98f24e
Antonio Hernández <antonio.hernandez@panamedia.net>
2015-12-22 12:15:05 +0100

    Fix rewrite

--- src/public/index.php
+++ src/public/index.php
@@ -2,7 +2,6 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-
 use EuroMillions\shared\config\bootstrap\WebLoader;
 use EuroMillions\shared\config\bootstrap\Bootstrap;
 
