e0cf79a5d8b252227ffd1406cc0a295104aabc67
rmesa <rmrbest@gmail.com>
2015-12-22 12:27:32 +0100

    cart page with react

--- src/react/src/cart.jsx
+++ src/react/src/cart.jsx
@@ -4,7 +4,14 @@ var ReactDOM = require('react-dom');
 
 var CartPage = new React.createClass({
 
+    render : function ()
+    {
+        return (
 
+        )
+    }
 
 
 });
+
+module.exports = CartPage;
