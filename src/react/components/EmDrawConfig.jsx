4b712e996ad4f8196fa2725e3be6099cfd0cf31a
rmesa <rmrbest@gmail.com>
2015-12-22 10:30:35 +0100

    date as default value

--- src/react/components/EmDrawConfig.jsx
+++ src/react/components/EmDrawConfig.jsx
@@ -21,11 +21,9 @@ var EuroMillionsDrawConfig = new React.createClass({
             options_draw_dates.push({text : obj, value : i});
         });
 
-        var default_value_draw = 2;
+        var default_value_draw = '2';
         var default_text_draw = 'Tuesday';
 
-        var default_value_drawdate = draw_dates[0];
-        var default_text_drawdate = draw_dates[0];
 
         var default_value_duration = '1';
         var default_text_duration = '1 week (Draw: 1)';
@@ -40,7 +38,7 @@ var EuroMillionsDrawConfig = new React.createClass({
 
 
         elem.push(<EmSelectDraw play_days={this.props.play_days} defaultValue={default_value_draw} defaultText={default_text_draw} {...this.props} active={this.state.selectdrawactive} key="1"/>)
-        elem.push(<EmSelectDrawDate change_date={this.props.date_play} defaultValue={default_value_drawdate} defaultText={default_text_drawdate} options={options_draw_dates} active={this.state.selectdrawactive} key="2"/>)
+        elem.push(<EmSelectDrawDate change_date={this.props.date_play} options={options_draw_dates} active={this.state.selectdrawactive} key="2"/>)
         elem.push(<EmSelectDrawDuration change_duration={this.props.duration} defaultValue={default_value_duration} defaultText={default_text_duration} options={options_draw_duration} active={this.state.selectdrawactive} key="3"/>)
         return (
             <div>
