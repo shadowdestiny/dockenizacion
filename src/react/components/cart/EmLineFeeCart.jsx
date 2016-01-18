e970d0cf716d6d7c88ee15f91b4d259fe527179a
Alessio Carone <littlemad@gmail.com>
2016-01-18 13:07:04 +0100

    Fix styling in cart

--- src/react/components/cart/EmLineFeeCart.jsx
+++ src/react/components/cart/EmLineFeeCart.jsx
@@ -46,10 +46,8 @@ var EmLineFeeCart = new React.createClass({
                 <div className="right tweak">
                     <div className="summary val">{fee_value}</div>
                     <div className="box-funds cl">
-                        <a className="add-funds" onClick={this.handleClick} href="javascript:void(0)">Add Funds to avoid charges</a><br></br>
-                        <div className="box-combo">
-                            {addFundComponent}
-                        </div>
+                        <a className="add-funds" onClick={this.handleClick} href="javascript:void(0)">Add Funds to avoid charges</a>
+                        {addFundComponent}
                     </div>
                 </div>
             </div>
