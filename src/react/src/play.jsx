a7f852055057e6507464eeca1e0ee2882c046479
rmrbest <rmrbest@gmail.com>
2016-01-24 20:05:16 +0100

    fix localstorage plays in IE

--- src/react/src/play.jsx
+++ src/react/src/play.jsx
@@ -137,10 +137,16 @@ var PlayPage = React.createClass({
             'numbers': numbers,
             'stars': stars
         };
-        localStorage.setItem('bet_line', JSON.stringify(this.state.storage, function(k,v){
-                return (v === null || v === 'null' ) ? { 'numbers' : [], 'stars' : []} : v;
+
+        for (var i = this.state.storage.length - 1; i >= 0; i--) {
+            if( this.state.storage[i] === 'undefined' || typeof this.state.storage[i] === 'undefined') {
+                this.state.storage[i] = { 'numbers' : [], 'stars' : []};
             }
-        ));
+        }
+        localStorage.setItem('bet_line', JSON.stringify(this.state.storage, function(k,v){
+                return (v == null || v == 'null') ? { 'numbers' : [], 'stars' : []} : v;
+            }));
+
         this.updatePrice();
     },
 
