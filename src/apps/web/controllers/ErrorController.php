c9f6963788df5d98167950328893989816d04261
Alessio Carone <littlemad@gmail.com>
2016-01-28 17:06:15 +0100

    Error 500 page

--- src/apps/web/controllers/ErrorController.php
+++ src/apps/web/controllers/ErrorController.php
@@ -3,5 +3,6 @@ namespace EuroMillions\web\controllers;
 class ErrorController extends PublicSiteControllerBase{
     
     public function page404Action(){}
+    public function page500Action(){}
 
 }
