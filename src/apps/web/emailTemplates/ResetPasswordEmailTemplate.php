502328538bf56117768f649ada8c8bb1167d9081
rmesa <rmrbest@gmail.com>
2016-02-02 11:10:39 +0100

    reset password email template

---
+++ src/apps/web/emailTemplates/ResetPasswordEmailTemplate.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace EuroMillions\web\emailTemplates;
+
+
+class ResetPasswordEmailTemplate extends EmailTemplateDecorator
+{
+
+    public function loadVars()
+    {
+        $vars = [
+            'template' => 'simple',
+            'subject' => 'Your password in Euromillions.com has been changed',
+            'vars' =>
+                [
+                    [
+                        'name'    => 'main',
+                        'content' => 'We have received a request to update your password. If you didn\'t make the request, please contact us at
+                                     <a href="mailto:support@euromilions.com">support@euromilions.com</a>'
+                    ],
+                ]
+        ];
+        return $vars;
+
+    }
+
+    public function loadHeader()
+    {
+        // TODO: Implement loadHeader() method.
+    }
+
+    public function loadFooter()
+    {
+        // TODO: Implement loadFooter() method.
+    }
+}
