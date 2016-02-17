4a7b900d4e332af5d2f66efce6213cb559f9896c
Alessio Carone <littlemad@gmail.com>
2016-02-17 12:31:45 +0100

    no message

--- src/react/components/cart/EmLineOrderConfig.jsx
+++ src/react/components/cart/EmLineOrderConfig.jsx
@@ -56,16 +56,10 @@ var EmLineOrderConfig = new React.createClass({
         return (
             <div className="row cl">
                 <div className="box-detail">
-                    <div className="desc">
-                        Draw
-                    </div>
-                    <div className="detail">
-                        {text_weeks}
-                    </div>
+                    <div className="desc">Draw</div>
+                    <div className="detail">{text_weeks}</div>
                 </div>
-                <div className="quantity">
-                    x10
-                </div>
+                <div className="quantity">x10</div>
             </div>
         )
     }
