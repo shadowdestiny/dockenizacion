169bcd1b741239deab0eff3274df4f28142abb3d
Antonio Hernández <antonio.hernandez@panamedia.net>
2016-02-23 15:59:17 +0100

    Fix migration script

--- dev-scripts/schema_migration.sh
+++ dev-scripts/schema_migration.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
 environment="$1"
 echo "executing schema migration on $1"
-if [ "$environment" == "devel" ]; then
+if [ "$environment" -eq "devel" ]; then
     cd /var/www
     now=$(date)
     echo "$now: Schema migration executed" >> /vagrant/dev-logs/migration.log
     vendor/bin/phinx migrate --configuration="phinx_schema.yml" -e devel
-elif [ "$environment" == "shippable" ]; then
-    echo "migrating..."
+elif [ "$environment" -eq "shippable" ] || [ "$environment" -eq "scrutinizer" ]; then
     cd src/
     vendor/bin/phinx migrate --configuration="phinx_schema.yml" -e shippable
-fi
-if [ "$environment" == "shippable" ]; then
     cd ..
 fi
 echo "finish migration"
