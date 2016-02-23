e0eed2e0bae2bf60374ce6c8f14baa0667055113
Antonio Hernández <antonio.hernandez@panamedia.net>
2016-02-23 12:50:08 +0100

    Trying scrutinizer config

--- dev-scripts/build_phalcon_scrutinizer.sh
+++ dev-scripts/build_phalcon_scrutinizer.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 git clone -q --depth=1 https://github.com/phalcon/cphalcon.git -b master
-cd cphalcon/ext; export CFLAGS="-g3 -O1 -fno-delete-null-pointer-checks -Wall"; phpize && ./configure --enable-phalcon && make -j4 && sudo make install && sed -i '$ a \\n[Phalcon]\nextension=phalcon.so\n'  /home/scrutinizer/.phpenv/versions/5.6.16/etc/php.ini
+cd cphalcon/ext; export CFLAGS="-g3 -O1 -fno-delete-null-pointer-checks -Wall"; phpize && ./configure --enable-phalcon && make -j4 && sudo make install
 cd ../..
