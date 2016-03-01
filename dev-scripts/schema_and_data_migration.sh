58ce89ad75714002fd95d9250cbd49dcbd93f623
Antonio Hernández <antonio.hernandez@panamedia.net>
2016-03-01 17:01:59 +0100

    Removing old folders no longer in use

--- dev-scripts/schema_and_data_migration.sh
+++ dev-scripts/schema_and_data_migration.sh
@@ -2,5 +2,3 @@
 cd /var/www
 vendor/bin/phinx migrate --configuration="phinx_schema.yml" -e $1
 vendor/bin/phinx migrate --configuration="phinx_data.yml" -e $1
-now=$(date)
-echo "$now: Schema and data migration executed" >> /vagrant/dev-logs/migration.log
