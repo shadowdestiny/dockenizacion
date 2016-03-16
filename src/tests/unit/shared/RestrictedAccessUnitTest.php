637a458851d9408848a9d329d363f352573c8227
rmesa <rmrbest@gmail.com>
2016-03-16 09:23:24 +0100

    restricted access and restrictionbyip unit tests

--- src/tests/unit/shared/RestrictedAccessUnitTest.php
+++ src/tests/unit/shared/RestrictedAccessUnitTest.php
@@ -1,10 +1,28 @@
 <?php
 namespace EuroMillions\tests\unit\shared;
 
+use EuroMillions\shared\components\RestrictedAccess;
 use EuroMillions\tests\base\UnitTestBase;
+use Prophecy\Argument;
 
 class RestrictedAccessUnitTest extends UnitTestBase 
 {
+
+    private $request_double;
+    private $response_double;
+    private $restrictedAccessConfig_double;
+    private $iRestrictedAccessStrategy_double;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->request_double = $this->prophesize('\Phalcon\Http\Request');
+        $this->response_double = $this->prophesize('\Phalcon\Http\Response');
+        $this->iRestrictedAccessStrategy_double = $this->getSharedInterfaceDouble('IRestrictedAccessStrategy');
+        $this->restrictedAccessConfig_double = $this->prophesize('EuroMillions\shared\dto\RestrictedAccessConfig');
+    }
+
+
     /**
      * method isRestricted
      * when called
@@ -12,7 +30,10 @@ class RestrictedAccessUnitTest extends UnitTestBase
      */
     public function test_isRestricted_called_returnStrategyResult()
     {
-        $this->markTestIncomplete();
+        $sut = new RestrictedAccess();
+        $this->iRestrictedAccessStrategy_double->isRestricted(Argument::any(),Argument::any())->willReturn(true);
+        $actual = $sut->isRestricted($this->iRestrictedAccessStrategy_double->reveal(), $this->request_double->reveal(),$this->restrictedAccessConfig_double->reveal());
+        $this->assertTrue($actual);
     }
     
 }
