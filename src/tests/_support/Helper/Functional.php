7abf00a4630fe33ca1570d6f7ff345a3ecb91677
Antonio Hernández <antonio.hernandez@panamedia.net>
2016-03-18 14:27:12 +0100

    Fixing Functional Helper

--- src/tests/_support/Helper/Functional.php
+++ src/tests/_support/Helper/Functional.php
@@ -4,9 +4,11 @@ namespace Helper;
 // here you can define custom actions
 // all public methods declared in helper class will be available in $I
 
+use Codeception\TestCase;
+
 class Functional extends \Codeception\Module
 {
-    public function _before()
+    public function _before(TestCase $test)
     {
         $command = '/vagrant/dev-scripts/run_seed.sh devel-test';
         exec($command);
