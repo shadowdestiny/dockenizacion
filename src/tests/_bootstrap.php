f96cf497b11ee26a6e374a831b2209fe9466dad0
Antonio Hernández <antonio.hernandez@panamedia.net>
2016-03-21 11:16:29 +0100

    Update codeception configurations for scrutinizer

--- src/tests/_bootstrap.php
+++ src/tests/_bootstrap.php
@@ -4,6 +4,8 @@ global $test_suite;
 define('TESTS_PATH', __DIR__.'/');
 define('APP_PATH', TESTS_PATH . '../apps/');
 
+$command = 'php '.__DIR__.'/../vendor/doctrine/orm/bin/doctrine orm:schema-tool:create --dump-sql > '.__DIR__.'/_data/dump.sql';
+exec($command);
 
 class EntityManagerFetcher
 {
--- src/tests/_support/Helper/Functional.php
+++ src/tests/_support/Helper/Functional.php
@@ -5,12 +5,15 @@ namespace Helper;
 // all public methods declared in helper class will be available in $I
 
 use Codeception\TestCase;
+use Phalcon\Di;
 
 class Functional extends \Codeception\Module
 {
     public function _before(TestCase $test)
     {
-        $command = '/vagrant/dev-scripts/run_seed.sh devel-test';
+        $di = DI::getDefault();
+        $environment = $di->get('environmentDetector')->get();
+        $command = '/vagrant/dev-scripts/run_seed.sh '.$environment;
         exec($command);
     }
 }
