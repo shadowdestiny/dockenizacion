17348d984a55a0b01414e775757d6e5b2eae9f87
rmesa <rmrbest@gmail.com>
2016-05-17 15:57:00 +0200

    refactoring numberscontroller

--- src/apps/web/vo/dto/EuroMillionsDrawDTO.php
+++ src/apps/web/vo/dto/EuroMillionsDrawDTO.php
@@ -12,8 +12,11 @@ class EuroMillionsDrawDTO extends DTOBase implements IDto
 {
 
     private $euroMillionsDraw;
+    public $id;
     public $regularNumbers;
+    public $regularNumbersArray;
     public $luckyNumbers;
+    public $luckyNumbersArray;
     public $jackpot;
     public $drawDate;
     public $euroMillionsDrawBreakDownDTO;
@@ -29,8 +32,11 @@ class EuroMillionsDrawDTO extends DTOBase implements IDto
 
     public function exChangeObject()
     {
+        $this->id = $this->euroMillionsDraw->getId();
         $this->regularNumbers = $this->euroMillionsDraw->getResult()->getRegularNumbers();
+        $this->regularNumbersArray = $this->euroMillionsDraw->getResult()->getRegularNumbersArray();
         $this->luckyNumbers = $this->euroMillionsDraw->getResult()->getLuckyNumbers();
+        $this->luckyNumbersArray = $this->euroMillionsDraw->getResult()->getLuckyNumbersArray();
         $this->jackpot = $this->euroMillionsDraw->getJackpot()->getAmount();
         $this->drawDate = $this->euroMillionsDraw->getDrawDate();
     }
