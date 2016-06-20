e3a7c9a380d6d4a0d68536ba6836670a90950a60
rmesa <rmrbest@gmail.com>
2016-06-20 12:34:07 +0200

    remove var_dump

--- src/apps/web/services/PlayService.php
+++ src/apps/web/services/PlayService.php
@@ -180,7 +180,7 @@ class PlayService
                     //error
                 }
             } catch ( \Exception $e ) {
-                var_dump($e->getMessage());die();
+
             }
         }
         return new ActionResult(false);
