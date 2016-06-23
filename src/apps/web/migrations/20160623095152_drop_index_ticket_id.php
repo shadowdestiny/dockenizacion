7caceb78d7f729990327a419fd4b4335ea4f1afe
rmesa <rmrbest@gmail.com>
2016-06-23 12:01:30 +0200

    drop index ticket id

---
+++ src/apps/web/migrations/20160623095152_drop_index_ticket_id.php
@@ -0,0 +1,34 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class DropIndexTicketId extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $this->execute('DROP INDEX IDX_7121252C68D879E3 ON matcher;
+                        ALTER TABLE matcher ADD CONSTRAINT FK_7121252C64B64DCC FOREIGN KEY (userId) REFERENCES users (id);
+        ');
+    }
+}
