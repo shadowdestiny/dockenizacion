ea0b9a77dd8e86424a6b5e14d4b897e99f3109b9
rmesa <rmrbest@gmail.com>
2016-09-28 10:38:00 +0200

    fix star 8 in responsive

--- src/apps/admin/controllers/AdminControllerBase.php
+++ src/apps/admin/controllers/AdminControllerBase.php
@@ -11,6 +11,9 @@ use Phalcon\Mvc\View;
 
 class AdminControllerBase extends Controller
 {
+
+    const ALLOW_IPS = ['62.57.159.66'];
+
     use PaginatedControllerTrait;
     /** @var  DomainAdminServiceFactory */
     protected $domainAdminServiceFactory;
@@ -25,8 +28,18 @@ class AdminControllerBase extends Controller
         $this->view->setRenderLevel(View::LEVEL_NO_RENDER);
     }
 
+    public function beforeExecuteRoute(\Phalcon\Mvc\Dispatcher $dispatcher) {
+       if($this->di->get('environmentDetector')->get() === 'production' ) {
+         if(!in_array($this->request->getClientAddress(), self::ALLOW_IPS)){
+             die('You have not access');
+         }
+       }
+    }
+
+
     public function afterExecuteRoute(\Phalcon\Mvc\Dispatcher $dispatcher)
     {
+
         $auth_user_service = $this->domainAdminServiceFactory->getAuthUserService();
         if($dispatcher->getControllerName() !== 'login' && !$auth_user_service->check_session()->success()) {
             return $this->response->redirect('/admin/login/index');
