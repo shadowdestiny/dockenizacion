bf9dbef53b5806512b678e36c3e666ae99c22716
Enric Tey <enrictey@gmail.com>
2016-11-04 09:13:18 +0100

    [EUR-795] - Auto-fill users name into credit card details form

---
+++ src/tests/functional/AutoFillCreditCardDetailsCest.php
@@ -0,0 +1,43 @@
+<?php
+
+class AutoFillCreditCardDetailsCest
+{
+    private $userId;
+    private $userName;
+    private $userSurname;
+
+    public function _before(FunctionalTester $I)
+    {
+        $user = $I->setRegisteredUser();
+        $this->userId = $user->getId();
+        $this->userName = $user->getName();
+        $this->userSurname = $user->getSurname();
+    }
+
+    public function _after(FunctionalTester $I)
+    {
+
+    }
+
+    /**
+     * @param FunctionalTester $I
+     * @group unic
+     */
+    public function redirectToLoginIfNotLoggedIn(FunctionalTester $I)
+    {
+        $I->amOnPage('/account');
+        $I->seeCurrentUrlMatches('/^\/sign-in/');
+    }
+
+    /**
+     * @param FunctionalTester $I
+     * @group unic
+     */
+    public function seeNameInCardHolderAtBalancePage(FunctionalTester $I)
+    {
+        $I->haveInSession('EM_current_user', $this->userId);
+        $I->amOnPage('/account/wallet');
+        $I->canSee('Enter your credit card details');
+        $I->assertNotEmpty($I->grabValueFrom('#card-holder'));
+    }
+}
