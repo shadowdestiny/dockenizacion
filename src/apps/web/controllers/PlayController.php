25e5a108681229871982f14c1f7a5dc91bb0f6c6
Enric Tey <enrictey@gmail.com>
2016-11-11 09:05:39 +0100

    [EUR-1200] - Countdown animation

--- src/apps/web/controllers/PlayController.php
+++ src/apps/web/controllers/PlayController.php
@@ -1,6 +1,7 @@
 <?php
 namespace EuroMillions\web\controllers;
 
+use EuroMillions\web\components\DateTimeUtil;
 use EuroMillions\web\components\tags\MetaDescriptionTag;
 use EuroMillions\web\components\ViewHelper;
 use EuroMillions\web\entities\User;
@@ -11,9 +12,13 @@ class PlayController extends PublicSiteControllerBase
     public function indexAction()
     {
         $jackpot = $this->userPreferencesService->getJackpotInMyCurrency($this->lotteryService->getNextJackpot('EuroMillions'));
+        $play_dates = $this->lotteryService->getRecurrentDrawDates('Euromillions');
+        $draw = $this->lotteryService->getNextDateDrawByLottery('Euromillions');
+        $date_time_util = new DateTimeUtil();
+        $dayOfWeek = $date_time_util->getDayOfWeek($draw);
+        $checkOpenTicket = $date_time_util->checkTimeForClosePlay($draw);
         $single_bet_price = $this->lotteryService->getSingleBetPriceByLottery('EuroMillions');
         $automatic_random = $this->request->get('random');
-        $drawData = $this->lotteryService->obtainDataForDraw('Euromillions');
 
         if(!$this->authService->isLogged()) {
             $user_currency = $this->userPreferencesService->getCurrency();
@@ -30,11 +35,11 @@ class PlayController extends PublicSiteControllerBase
 
         return $this->view->setVars([
             'jackpot_value' => ViewHelper::formatJackpotNoCents($jackpot),
-            'play_dates' => $drawData['playDates'],
-            'next_draw' => $drawData['dayOfWeek'],
-            'next_draw_format' => $drawData['drawDate'],
+            'play_dates' => $play_dates,
+            'next_draw' => $dayOfWeek,
+            'next_draw_format' => $draw->format('l j M G:i'),
             'currency_symbol' => $currency_symbol,
-            'openTicket' => 0,
+            'openTicket' => ($checkOpenTicket)  ? '1': '0',
             'single_bet_price' => $single_bet_price_currency->getAmount() /100,
             'automatic_random' => isset($automatic_random) ? true : false,
         ]);
