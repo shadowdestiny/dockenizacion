8e1adb1dd1ce2f7dda457ea71985608bb2a56fae
benairs3 <benairs3@gmail.com>
2016-12-02 14:17:11 +0100

    [EUR-1226] - Create component draws/price

--- src/react/components/EmDrawTotalPrice.jsx
+++ src/react/components/EmDrawTotalPrice.jsx
@@ -6,8 +6,8 @@ var EuroMillionsDiscountLines = new React.createClass({
         var total_price = this.props.num_lines * this.props.draws_number;
 
         return (
-            <span>
-                <h1>TOTAL: {this.props.num_lines} Lines x {this.props.draws_number} Draws = {total_price} &euro;</h1>
+            <span className="drawTotal">
+                TOTAL: {this.props.num_lines} Lines x {this.props.draws_number} Draws = {total_price} &euro;
             </span>
         )
     }
