2acea98a2624ccdce8c0fab538d377a6f9643a6d
benairs3 <benairs3@gmail.com>
2017-04-26 15:12:53 +0200

    [EUR-1352] - Default language

--- src/apps/web/repositories/TranslationDetailRepository.php
+++ src/apps/web/repositories/TranslationDetailRepository.php
@@ -1,25 +1,41 @@
 <?php
+
 namespace EuroMillions\web\repositories;
 
 use Doctrine\ORM\EntityRepository;
 
 class TranslationDetailRepository extends EntityRepository
 {
+    //TODO:set cache and default language
     public function getTranslation($language, $key)
     {
         $result = $this->getEntityManager()
             ->createQuery(
                 'SELECT td.value'
-                .' FROM '.$this->getEntityName().' td JOIN td.translation t'
-                .' WHERE td.lang = :lang AND t.translationKey = :key')
+                . ' FROM ' . $this->getEntityName() . ' td JOIN td.translation t'
+                . ' WHERE td.lang = :lang AND t.translationKey = :key')
             ->setMaxResults(1)
-            ->setParameters(['lang' => $language, 'key'=>$key])
-            ->useResultCache(true)
+            ->setParameters(['lang' => $language, 'key' => $key])
+            ->useResultCache(true, 3600)
             ->getResult();
-        //TODO: if empty return default language
+
         if ($result) {
             return $result[0]['value'];
         } else {
+            $result = $this->getEntityManager()
+                ->createQuery(
+                    'SELECT td.value'
+                    . ' FROM ' . $this->getEntityName() . ' td JOIN td.translation t'
+                    . ' WHERE td.lang = :lang AND t.translationKey = :key')
+                ->setMaxResults(1)
+                ->setParameters(['lang' => 'en', 'key' => $key])
+                ->useResultCache(true, 3600)
+                ->getResult();
+
+            if ($result) {
+                return $result[0]['value'];
+            }
+
             return $key;
         }
     }
