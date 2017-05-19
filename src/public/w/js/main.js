ba77d0c2d8076966ea47770acf79a185dd1fff14
Enric Tey <enrictey@gmail.com>
2017-05-19 09:57:42 +0200

    [EUR-1365] - Live Chat and ticketing system

--- src/public/w/js/main.js
+++ src/public/w/js/main.js
@@ -37,14 +37,17 @@ var globalFunctions = {
     }
 };
 
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function()
+    { var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0]; s1.async=true; s1.src='https://embed.tawk.to/591c2e4c64f23d19a89b2788/default'; s1.charset='UTF-8'; s1.setAttribute('crossorigin','*'); s0.parentNode.insertBefore(s1,s0); }
+)();
+
 function btnShowHide(button, show, hide){
     $(button).click(function(){
         $(show).show();
         $(hide).hide();
     });
 }
-
-
 
 function selectFix(){ // Style the "Select"
     if('querySelector' in document && 'addEventListener' in window){
