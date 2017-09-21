b8c02946962423ab7dd782304fed62fa34cff8f6
benairs3 <benairs3@gmail.com>
2017-09-21 10:04:21 +0200

    [EUR-1413] - Change Page names on Mobile menu

--- src/public/w/js/main.js
+++ src/public/w/js/main.js
@@ -208,26 +208,23 @@ $(function(){
     $(window).resize(navLanguage());
 
     /* Hide Currency after tapping on mobile */
-    $('html').on('touchstart', function(e) {
-        if ($('.div-currency').is(":visible")) {
-            $('.div-currency').hide();
-        } else {
-            if (e.target.className.split(" ")[1] == "myCur") {
-                $('.div-currency').show();
-            }
-            ;
-        }
-    });
-
     $('html').on('touchstart', function(e){
+        if($('.div-currency').is(":visible")){
+            $('.div-currency').hide();
+        }else{
+            if(e.target.className.split(" ")[1] == "myCur"){
+                $('.div-currency').show();
+            };
+        }
+
         if($('.div-language').is(":visible")){
             $('.div-language').hide();
         }else{
-            if(e.target.className.split(" ")[1] == "myLang"){
+            if(e.target.className.split( )[1] == "myLang"){
                 $('.div-language').show();
             };
         }
-    });
+    })
     $(".div-currency").on('touchstart',function(e){
         e.stopPropagation();
     });
