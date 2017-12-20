ff0526b50a81851d4ff3f1c516a7c9d8e0159738
benairs3 <benairs3@gmail.com>
2017-12-20 14:13:14 +0100

    [BUG] - Chistmas Tickets don't work
    (cherry picked from commit 27799f2)

--- src/apps/web/services/external_apis/LotteryValidationCastilloChristmasApi.php
+++ src/apps/web/services/external_apis/LotteryValidationCastilloChristmasApi.php
@@ -49,9 +49,10 @@ class LotteryValidationCastilloChristmasApi
         $this->castilloId = $castilloTicketId;
         $luckyNumbers = explode(',', $line->getLuckyNumbers());
         $content = "<?xml version='1.0' encoding='UTF-8'?>";
+//        <date>" . $date_next_draw->format('Ymd') . "</date>
         $content .= "<lottery number='1' price='" . self::PRICE_BET . "'>
             <decimo>
-                <date>" . $date_next_draw->format('Ymd') . "</date>
+                <date>20171222</date>
                 <number>" . str_replace(',', '', $line->getRegularNumbers()) . "</number>
                 <serie>" . $luckyNumbers[0] . "</serie>
                 <fraccion>" . $luckyNumbers[1] . "</fraccion>
