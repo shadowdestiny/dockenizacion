d2ef6387332705f2dcf2c50c2a6e1b59391a323b
benairs3 <benairs3@gmail.com>
2018-01-25 10:42:30 +0100

    [EUR-1560] - How to - Powerball widget

--- src/apps/web/controllers/FaqController.php
+++ src/apps/web/controllers/FaqController.php
@@ -3,6 +3,7 @@
 namespace EuroMillions\web\controllers;
 
 use EuroMillions\web\components\tags\MetaDescriptionTag;
+use EuroMillions\web\components\ViewHelper;
 use EuroMillions\web\entities\Lottery;
 use EuroMillions\shared\vo\results\ActionResult;
 
@@ -16,9 +17,9 @@ class FaqController extends PublicSiteControllerBase
 
         $this->tag->prependTitle($this->languageService->translate('faq_name'));
         MetaDescriptionTag::setDescription($this->languageService->translate('faq_desc'));
-
         return $this->view->setVars([
             'price_bet' => (!empty($lottery)) ? $lottery->getSingleBetPrice()->getAmount() / 10000 : "",
+            'jackpot_value' => ViewHelper::formatJackpotNoCents($this->userPreferencesService->getJackpotInMyCurrencyAndMillions($this->lotteryService->getNextJackpot('EuroMillions'))),
             'draw_time' => (!empty($lottery)) ? $lottery->getDrawTime() : '',
             'email_support' => $config->email_support['email'],
             'pageController' => 'euroFaq',
