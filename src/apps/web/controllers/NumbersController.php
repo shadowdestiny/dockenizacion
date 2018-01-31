edb7679bd6e4e2cb46bb0b42da0a95294905bd1c
Enric Tey <enrictey@gmail.com>
2018-01-31 14:45:17 +0100

    [EUR-1533] - New Design - EuroMillions draw history page

--- src/apps/web/controllers/NumbersController.php
+++ src/apps/web/controllers/NumbersController.php
@@ -59,8 +59,9 @@ class NumbersController extends PublicSiteControllerBase
 
         $this->view->pick('/numbers/past');
         return $this->view->setVars([
-            'jackpot_value' => $this->userPreferencesService->getJackpotInMyCurrency($this->lotteryService->getNextJackpot('EuroMillions')),
+            'jackpot_value' => ViewHelper::formatJackpotNoCents($this->userPreferencesService->getJackpotInMyCurrencyAndMillions($this->lotteryService->getNextJackpot('EuroMillions'))),
             'date_draw' => $this->lotteryService->getNextDateDrawByLottery('EuroMillions')->modify('-1 hours')->format('Y-m-d H:i:s'),
+            'show_s_days' => (new \DateTime())->diff($this->lotteryService->getNextDateDrawByLottery('EuroMillions')->modify('-1 hours'))->format('%a'),
             'symbol' => $this->userPreferencesService->getMyCurrencyNameAndSymbol()['symbol'],
             'list_draws' => $result->getValues(),
             'pageController' => 'euroPastResult',
@@ -78,11 +79,6 @@ class NumbersController extends PublicSiteControllerBase
         $actualDate = new \DateTime();
         $date = empty($date) ? new \DateTime() : new \DateTime($date);
         $draw_result = $this->lotteryService->getDrawWithBreakDownByDate($lotteryName, $date);
-        if (!$draw_result->success()) {
-            return $this->dispatcher->forward([
-                'action' => 'pastList'
-            ]);
-        }
         /** @var EuroMillionsDraw $euroMillionsDraw */
         $euroMillionsDraw = $draw_result->getValues();
         $breakDownDTO = new EuroMillionsDrawBreakDownDTO($euroMillionsDraw->getBreakDown());
