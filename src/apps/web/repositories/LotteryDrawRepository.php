bac5fb699b746e3c683ae019aed4d9379f373c1b
benairs3 <benairs3@gmail.com>
2018-02-01 11:25:57 +0100

    [EUR-1568] - Add rules to get Eruomillions Results

--- src/apps/web/repositories/LotteryDrawRepository.php
+++ src/apps/web/repositories/LotteryDrawRepository.php
@@ -22,7 +22,7 @@ class LotteryDrawRepository extends EntityRepository
                 . ' ORDER BY ld.draw_date DESC')
             ->setMaxResults(1)
             ->setParameters(['lottery_name' => $lotteryName, 'date' => $date])
-            ->useResultCache(true)
+            ->useResultCache(true, 3600)
             ->getResult();
         return $result[0]->getJackpot();
     }
@@ -90,7 +90,7 @@ class LotteryDrawRepository extends EntityRepository
                 . ' WHERE l.name = :lottery_name AND ld.draw_date = :date')
             ->setMaxResults(1)
             ->setParameters(['lottery_name' => $lottery->getName(), 'date' => $draw_date->format('Y-m-d')])
-            ->useResultCache(true)
+            ->useResultCache(true, 3600)
             ->getResult();
         if (!count($result)) {
             throw new DataMissingException('Couldn\'t find the last result in the database');
@@ -112,7 +112,7 @@ class LotteryDrawRepository extends EntityRepository
                 . ' WHERE l.name = :lottery_name AND ld.draw_date = :date')
             ->setMaxResults(1)
             ->setParameters(['lottery_name' => $lottery->getName(), 'date' => $draw_date->format('Y-m-d')])
-            ->useResultCache(true)
+            ->useResultCache(true, 3600)
             ->getResult();
         if (!count($result)) {
             throw new DataMissingException('Couldn\'t find the last result in the database');
@@ -210,7 +210,7 @@ class LotteryDrawRepository extends EntityRepository
                 . ' WHERE l.name = :lottery_name AND ld.draw_date = :date')
             ->setMaxResults(1)
             ->setParameters(['lottery_name' => $lottery->getName(), 'date' => $draw_date->format('Y-m-d')])
-            ->useResultCache(true)
+            ->useResultCache(true, 3600)
             ->getResult();
         return $result[0];
     }
