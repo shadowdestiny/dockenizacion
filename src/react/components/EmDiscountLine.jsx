fc6cf0a5dfdfcbafc7c34a0f0b9931de898fc8b8
benairs3 <benairs3@gmail.com>
2018-02-08 17:46:16 +0100

    [EUR-1586] - Add keys and review content/headings - Play page

--- src/react/components/EmDiscountLine.jsx
+++ src/react/components/EmDiscountLine.jsx
@@ -11,7 +11,7 @@ var EuroMillionsDiscountLine = React.createClass({
                 <a className={classBtn} href="javascript:void(0);" onClick={this.props.sendLineSelected.bind(null, this.props.draws, this.props.discount)}>
                     <span className="top">{this.props.desc}</span>
                     <span className="bottom">
-                        {this.props.price_desc}
+                        {this.props.price / 100}{this.props.price_desc}
                     </span>
                 </a>
             </div>
