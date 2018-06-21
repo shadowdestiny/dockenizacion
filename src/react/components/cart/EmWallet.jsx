e1465de8236152fb9520575d29f70cc5bc6a20bb
benairs3 <benairs3@gmail.com>
2018-06-21 17:23:50 +0200

    [EUR-1697] - Create the Power Play Functionality

--- src/react/components/cart/EmWallet.jsx
+++ src/react/components/cart/EmWallet.jsx
@@ -40,9 +40,6 @@ var EmWallet = new React.createClass({
         var wallet_balance = (this.state.checked) ? this.props.wallet_balance : 0;
         var total_price = this.props.total_price;
 
-console.log(wallet_balance);
-        console.log(total_price);
-
         if(this.props.powerplay) {
             total_price = parseFloat(total_price) + ((parseFloat(this.props.total_lines) * parseFloat(this.props.powerplayprice)) * frequency);
         }
@@ -52,15 +49,8 @@ console.log(wallet_balance);
 
         var disabled_value = (this.state.checked) ? 'summary val' : 'summary val disabled';
         var operand_value = (this.state.checked) ? ' - ' : ' ';
-        // console.log(wallet);
-        // if(this.props.powerplay && wallet > 0) {
-        //      wallet_value = parseFloat(wallet) + ((parseFloat(this.props.total_lines) * parseFloat(this.props.powerplayprice)) * frequency);
-        // }
-        // alert(wallet_balance);
         var value = accounting.formatMoney(wallet_value, this.props.currency_symbol, 2);
-        // alert(value);
         var total_value = this.props.symbol_position ? operand_value + ' ' + value  :  operand_value + ' ' + value;
-        // alert(value);
         return (
             <div className="row cl">
 
