bf430095071f37b1dea9363895ab451c63a005b4
benairs3 <benairs3@gmail.com>
2018-07-03 13:21:01 +0200

    [EUR-1314] - PowerBall - Emails - Purchase Subscription Confirmation

--- src/apps/web/emailTemplates/PowerBallPurchaseConfirmationEmailTemplate.php
+++ src/apps/web/emailTemplates/PowerBallPurchaseConfirmationEmailTemplate.php
@@ -68,9 +68,11 @@ class PowerBallPurchaseConfirmationEmailTemplate extends EmailTemplateDecorator
             foreach($line->getLine()->getRegularNumbersArray() as $balls) {
                 $play['regular_numbers'][]['number'] = $balls;
             }
-            foreach($line->getLine()->getLuckyNumbersArray() as $stars) {
-                $play['lucky_numbers'][]['number'] = $stars;
-            }
+            $lucky = $line->getLine()->getLuckyNumbersArray();
+            $play['lucky_numbers'][]['number'] = $lucky[1];
+//            foreach($line->getLine()->getLuckyNumbersArray() as $stars) {
+//                $play['lucky_numbers'][]['number'] = $stars;
+//            }
             array_push($lines,$play);
         }
         return $lines;
