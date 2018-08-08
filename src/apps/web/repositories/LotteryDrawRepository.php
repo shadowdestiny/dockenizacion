9383ce9e79fa4182bd7363ee973b474e9823d0bf
benairs3 <benairs3@gmail.com>
2018-08-08 16:06:58 +0200

    [EUR-1823] - Jackpot key on powerball page

--- src/apps/web/repositories/LotteryDrawRepository.php
+++ src/apps/web/repositories/LotteryDrawRepository.php
@@ -60,10 +60,10 @@ class LotteryDrawRepository extends EntityRepository
         }
         $next_draw_date = $lottery->getNextDrawDate($date);
         if($lottery->getName() == 'PowerBall') {
-            $next_draw_date = DateTimeUtil::convertDateTimeBetweenTimeZones($next_draw_date,'America/New_York','Europe/Madrid');
+            $next_draw_date = DateTimeUtil::convertDateTimeBetweenTimeZones($next_draw_date,'America/New_York','Europe/Madrid')->modify('-1 day');
         }
 
-
+//        var_dump($next_draw_date);die();
         /** @var EuroMillionsDraw[] $result */
         $result = $this->getEntityManager()
             ->createQuery(
