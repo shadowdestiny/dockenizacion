9479b336f6c9d56a9fcd9463f18611a1ac63d6b7
rmrbest <rmrbest@gmail.com>
2018-10-10 09:56:40 +0200

    staging config changes

--- src/apps/shared/services/payments_load_balancer_strategies/GeoIPStrategy.php
+++ src/apps/shared/services/payments_load_balancer_strategies/GeoIPStrategy.php
@@ -82,6 +82,10 @@ class GeoIPStrategy implements ILoadBalancingPayment
         }
         else
         {
+            if(!isset($_SERVER['REMOTE_ADDR']))
+            {
+                $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
+            }
             $ip=$_SERVER['REMOTE_ADDR'];
         }
         return $ip;
