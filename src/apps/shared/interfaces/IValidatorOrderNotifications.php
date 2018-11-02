96062926356ce594064e0791b3bdbf855d33c750
rmrbest <rmrbest@gmail.com>
2018-11-02 10:23:25 +0100

    order actions and order validators

---
+++ src/apps/shared/interfaces/IValidatorOrderNotifications.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rmrbest
+ * Date: 29/10/18
+ * Time: 11:11
+ */
+
+namespace EuroMillions\shared\interfaces;
+
+
+interface IValidatorOrderNotifications
+{
+
+    public function validate();
+
+
+}
--- src/apps/web/services/external_apis/LoteriasyapuestasDotEsApi.php
+++ src/apps/web/services/external_apis/LoteriasyapuestasDotEsApi.php
@@ -46,7 +46,6 @@ class LoteriasyapuestasDotEsApi implements IResultApi, IJackpotApi
      */
     public function getJackpotForDate($lotteryName, $date)
     {
-        throw new ValidDateRangeException('The date requested ('.$date.') is not valid for the LoteriasyapuestasDotEsApi');
         $response = $this->curlWrapper->get('http://www.loteriasyapuestas.es/es/euromillones/botes/.formatoRSS');
         $xml = new \SimpleXMLElement($response->body);
         foreach ($xml->channel->item as $item) {
