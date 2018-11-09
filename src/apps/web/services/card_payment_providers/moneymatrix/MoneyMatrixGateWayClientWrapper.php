4020e1f9c496ab7748fa337e476e1883115e5772
David C <david.corto@panamedia.eu>
2018-11-09 08:55:26 +0100

    Undo change

---
+++ src/apps/web/services/card_payment_providers/moneymatrix/MoneyMatrixGateWayClientWrapper.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace EuroMillions\web\services\card_payment_providers\moneymatrix;
+
+
+use Phalcon\Http\Client\Provider\Curl;
+use Phalcon\Http\Client\Response;
+
+class MoneyMatrixGatewayClientWrapper
+{
+
+    private $config;
+
+    private $curlWrapper;
+
+    public function __construct(MoneyMatrixConfig $config, Curl $curlWrapper = null)
+    {
+        $this->config = $config;
+        $this->curlWrapper = $curlWrapper ? $curlWrapper : new Curl();
+    }
+
+    public function send($params,$action) {
+        try {
+            /** @var Response  $response */
+            $this->curlWrapper->setOption(CURLOPT_SSL_VERIFYHOST,false);
+            $this->curlWrapper->setOption(CURLOPT_SSL_VERIFYPEER,false);
+            $response = $this->curlWrapper->post($this->config->getEndpoint().'/'.$action,
+                $params,
+                true,
+                array(
+                    "Content-Type: application/json; charset=utf-8",
+                )
+            );
+            return $response->body;
+        } catch ( \Exception $e ) {
+            throw new \Exception($e->getMessage());
+        }
+
+    }
+
+}
