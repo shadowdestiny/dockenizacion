851a2ccf5407402da116bd90959c772cf3f6cea3
Victor Prato <victorprato@gmail.com>
2018-11-24 11:13:39 -0400

    fixing refactor code on userAward entity

--- src/apps/shared/components/transactionBuilders/WinningTransactionDataBuilder.php
+++ src/apps/shared/components/transactionBuilders/WinningTransactionDataBuilder.php
@@ -99,7 +99,7 @@ class WinningTransactionDataBuilder implements IBuildTransactionData
             $this->type = TransactionType::BIG_WINNING;
         }
         else{
-            $this->user->awardPrize($this->winning->getPrice());
+            $this->user->awardPrize($this->winning);
             $this->data['walletAfter'] = $this->user->getWallet();
             $this->data['state'] = '';
             $this->data['lottery_id'] = $this->winning->getLotteryId();
