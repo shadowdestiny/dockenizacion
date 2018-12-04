4adcb3b34db693ac6ecf91874bf719de42412574
david.corto <david.corto@panamedia.eu>
2018-12-04 11:28:38 +0100

    WIP: Cleanup jenkinsfile

---
+++ devOps/ci/test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+sudo docker login --username $DOCKER_HUB_USER --password $DOCKER_HUB_PASSWORD
+sudo docker pull panamedialottery/euromillions-php:tools
+
+sudo docker build --target tools -f ${WORKSPACE}/devOps/docker/php/Dockerfile -t panamedialottery/euromillions-php:tools ${WORKSPACE}
+sudo docker run --rm -v ${WORKSPACE}/src:/var/www panamedialottery/euromillions-php:tools composer install --no-progress
+
+sudo docker-compose -p test -f ${WORKSPACE}/docker-compose.test.yml build
+sudo docker-compose -p test -f ${WORKSPACE}/docker-compose.test.yml up -d
+sudo docker-compose -p test -f ${WORKSPACE}/docker-compose.test.yml exec -T php ./vendor/bin/phpunit --log-junit tests/junit.xml -c tests/phpunit.xml
