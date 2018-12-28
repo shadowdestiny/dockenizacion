6da7694474636acc48b39090b4e8268ac76ff9c7
Luis Marin <lrm.programador@gmail.com>
2018-12-28 12:11:55 -0400

    requirement:: adaptation of the new eurojackpot module

--- src/apps/shared/components/widgets/LotteryResultsListWidget.php
+++ src/apps/shared/components/widgets/LotteryResultsListWidget.php
@@ -64,6 +64,8 @@ class LotteryResultsListWidget extends \Phalcon\Mvc\User\Component
                 return 'lottery-result--powerball';
             case 'Christmas'   :
                 return 'lottery-result--christmas';
+            case 'EuroJackpot'   :
+                return 'lottery-result--christmas';
         }
     }
 
