f2694af15ae3ce836c62b0f7ba2420131fbb33b9
david.corto <david.corto@panamedia.eu>
2019-01-04 09:20:06 +0100

    Tag cron docker image by environment on deploy

--- devOps/ci/deploy.sh
+++ devOps/ci/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-sudo docker tag panamedialottery/euromillions-php:cron_${BUILD_IMAGE_TAG} panamedialottery/euromillions-php:cron
-sudo docker push panamedialottery/euromillions-php:cron
+sudo docker tag panamedialottery/euromillions-php:cron_${BUILD_IMAGE_TAG} panamedialottery/euromillions-php:cron_${EM_ENVIRONMENT}
+sudo docker push panamedialottery/euromillions-php:cron_${EM_ENVIRONMENT}
 
 sudo docker tag panamedialottery/euromillions-php:${BUILD_IMAGE_TAG} panamedialottery/euromillions-php:${EM_ENVIRONMENT}
 sudo docker push panamedialottery/euromillions-php:${EM_ENVIRONMENT}
