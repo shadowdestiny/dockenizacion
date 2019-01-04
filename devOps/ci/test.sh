06bb6946e993999213dba0b01d01ef611598cf34
david.corto <david.corto@panamedia.eu>
2019-01-04 10:57:41 +0100

    Push php tool docker image to registry after build for tests run.

--- devOps/ci/test.sh
+++ devOps/ci/test.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
 sudo docker login --username $DOCKER_HUB_USER --password $DOCKER_HUB_PASSWORD
+
 sudo docker pull panamedialottery/euromillions-php:tools
 
 sudo docker build --target tools -f ${WORKSPACE}/devOps/docker/php/Dockerfile -t panamedialottery/euromillions-php:tools ${WORKSPACE}
+
+sudo docker push panamedialottery/euromillions-php:tools
+
 sudo docker run --rm -v ${WORKSPACE}/src:/var/www panamedialottery/euromillions-php:tools composer install --no-progress
 
 sudo docker-compose -p test -f ${WORKSPACE}/docker-compose.test.yml build
