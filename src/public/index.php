cc4d89e20251ddf636ff4127f32a8381cb5ac5e2
David Corto <david.corto@panamedia.eu>
2019-02-04 08:57:13 +0100

    Clean old infrastructure code, legacy vagrant config and old docker setup.

--- src/public/index.php
+++ src/public/index.php
@@ -4,7 +4,6 @@ use EuroMillions\shared\config\bootstrap\Bootstrap;
 $public_path = __DIR__;
 $app_web_path = $public_path . '/../apps/web';
 $app_path = $public_path . '/../apps/';
-$global_config_path = $app_path . '../global_config/';
 define('APP_PATH', $app_path);
 
 $config_path = $app_path . 'shared/config/';
