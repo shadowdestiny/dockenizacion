8ecd54d0436c3a0edbb1fd31dd22678f586a5b93
Wilcar J. Angulo <wilcarjose@gmail.com>
2019-02-12 19:59:34 -0400

    DataProvider is used in WinEmailEnumCest

--- src/tests/unit/WinEmailEnumCest.php
+++ src/tests/unit/WinEmailEnumCest.php
@@ -5,18 +5,27 @@ use EuroMillions\shared\enums\WinEmailEnum;
 class WinEmailEnumCest
 {
     /**
+     * @var EuroMillions\shared\enums\WinEmailEnum
+     */
+    protected $winEmailEnum;
+
+    public function _before(\UnitTester $I)
+    {
+        $this->winEmailEnum = new WinEmailEnum();
+    }
+
+    /**
      * method findTemplatePathByLotteryName
      * when hasLotteryName
      * should returnTheCorrectTemplate
      * @param UnitTester $I
      * @group email-enum
+     * @dataProvider getSmallTemplate
      */
-    public function test_findTemplatePathByLotteryName_hasLotteryName_returnTheCorrectTemplate(UnitTester $I)
+    public function test_findTemplatePathByLotteryName_hasLotteryName_returnTheCorrectTemplate(UnitTester $I, \Codeception\Example $data)
     {
-        foreach ($this->getSmallTemplate() as $lotteryName => $expectedTemplate) {
-            $template = (new WinEmailEnum())->findTemplatePathByLotteryName($lotteryName, $isBig = false);
-            $I->assertEquals($template, $expectedTemplate);
-        }
+        $template = $this->winEmailEnum->findTemplatePathByLotteryName($data['lottery'], $isBig = false);
+        $I->assertEquals($template, $data['template']);
     }
 
     /**
@@ -25,13 +34,12 @@ class WinEmailEnumCest
      * should returnTheCorrectAboveTemplate
      * @param UnitTester $I
      * @group email-enum
+     * @dataProvider getBigTemplate
      */
-    public function test_findTemplatePathByLotteryName_hasLotteryName_returnTheCorrectAboveTemplate(UnitTester $I)
+    public function test_findTemplatePathByLotteryName_hasLotteryName_returnTheCorrectAboveTemplate(UnitTester $I, \Codeception\Example $data)
     {
-        foreach ($this->getBigTemplate() as $lotteryName => $expectedTemplate) {
-            $template = (new WinEmailEnum())->findTemplatePathByLotteryName($lotteryName, $isBig = true);
-            $I->assertEquals($template, $expectedTemplate);
-        }
+        $template = $this->winEmailEnum->findTemplatePathByLotteryName($data['lottery'], $isBig = true);
+        $I->assertEquals($template, $data['template']);
     }
 
     /**
@@ -44,30 +52,36 @@ class WinEmailEnumCest
     public function test_findTemplatePathByLotteryName_hasFakeLotteryName_throwUnexpectedValueException(UnitTester $I)
     {
         try {
-            (new WinEmailEnum())->findTemplatePathByLotteryName('FakeLottery');
+            $this->winEmailEnum->findTemplatePathByLotteryName('FakeLottery');
         }
         catch (Exception $e) {
             $I->assertTrue($e instanceof \UnexpectedValueException);
         }
     }
 
+    /**
+     * @return array
+     */
     protected function getSmallTemplate()
     {
         return [
-            'EuroMillions' => 'EuroMillions\web\emailTemplates\WinEmailTemplate',
-            'PowerBall' => 'EuroMillions\web\emailTemplates\WinEmailPowerBallTemplate',
-            'MegaMillions' => 'EuroMillions\megamillions\emailTemplates\WinEmailMegaMillionsTemplate',
-            'EuroJackpot' => 'EuroMillions\eurojackpot\emailTemplates\WinEmailEuroJackpotTemplate',
+            ['lottery' => 'EuroMillions',   'template' => 'EuroMillions\web\emailTemplates\WinEmailTemplate'],
+            ['lottery' => 'PowerBall',      'template' => 'EuroMillions\web\emailTemplates\WinEmailPowerBallTemplate'],
+            ['lottery' => 'MegaMillions',   'template' => 'EuroMillions\megamillions\emailTemplates\WinEmailMegaMillionsTemplate'],
+            ['lottery' => 'EuroJackpot',    'template' => 'EuroMillions\eurojackpot\emailTemplates\WinEmailEuroJackpotTemplate'],
         ];
     }
 
+    /**
+     * @return array
+     */
     protected function getBigTemplate()
     {
         return [
-            'EuroMillions' => 'EuroMillions\web\emailTemplates\WinEmailAboveTemplate',
-            'PowerBall' => 'EuroMillions\web\emailTemplates\WinEmailPowerBallAboveTemplate',
-            'MegaMillions' => 'EuroMillions\megamillions\emailTemplates\WinEmailMegaMillionsAboveTemplate',
-            'EuroJackpot' => 'EuroMillions\eurojackpot\emailTemplates\WinEmailEuroJackpotAboveTemplate',
+            ['lottery' => 'EuroMillions',   'template' => 'EuroMillions\web\emailTemplates\WinEmailAboveTemplate'],
+            ['lottery' => 'PowerBall',      'template' => 'EuroMillions\web\emailTemplates\WinEmailPowerBallAboveTemplate'],
+            ['lottery' => 'MegaMillions',   'template' => 'EuroMillions\megamillions\emailTemplates\WinEmailMegaMillionsAboveTemplate'],
+            ['lottery' => 'EuroJackpot',    'template' => 'EuroMillions\eurojackpot\emailTemplates\WinEmailEuroJackpotAboveTemplate'],
         ];
     }
 }
