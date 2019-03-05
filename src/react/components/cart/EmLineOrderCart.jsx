807b5f5025eef503cbba578312c3d5327a558346
Luis Marin <lrm.programador@gmail.com>
2019-03-05 15:48:03 -0400

    It is changed sixth by Lucky in the views

--- src/react/components/cart/EmLineOrderCart.jsx
+++ src/react/components/cart/EmLineOrderCart.jsx
@@ -50,11 +50,7 @@ var EmLineOrder = new React.createClass({
                             )
                         }
                         {
-                            (this.props.megasena  ?
-                                    stars.map(function(star,i) {
-                                        return <li key={i}>{star}</li>})
-                                    : ""
-                            )
+                            (this.props.megasena  ? <li>{stars[1]}</li> : "")
                         }
                         {
                             (!this.props.megamillions && !this.props.powerball && !this.props.eurojackpot && !this.props.megasena ?
