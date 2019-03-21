354d073821531c4e9526d9d72c3c5672a2760704
rmrbest <calesti2980>
2019-03-21 11:03:46 +0100

    fix(withdraw): remove a new withdrawal transaction. It does not make sense

--- src/apps/web/services/WalletService.php
+++ src/apps/web/services/WalletService.php
@@ -347,7 +347,6 @@ class WalletService
     public function withDraw(User $user, Money $amount)
     {
         try {
-            $walletBefore = $user->getWallet();
             $newWallet = $user->getWallet()->withdraw($amount);
             if ($newWallet == null) {
                 throw new \Exception('You don\'t have enough winning amount to complete transaction');
@@ -355,15 +354,6 @@ class WalletService
             $user->setWallet($newWallet);
             $this->entityManager->persist($user);
             $this->entityManager->flush();
-            $data = [];
-            $data['now'] = new \DateTime();
-            $data['walletBefore'] = $walletBefore;
-            $data['walletAfter'] = $user->getWallet();
-            $data['user'] = $user;
-            $data['accountBankId'] = '1';
-            $data['amountWithdrawed'] = $amount->getAmount();
-            $data['state'] = 'pending';
-            $this->transactionService->storeTransaction(TransactionType::WINNINGS_WITHDRAW, $data);
             return new ActionResult(true);
         } catch (\Exception $e) {
             return new ActionResult(false, $e->getMessage());
