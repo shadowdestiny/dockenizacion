49dc76b8c89d67996bb7c09189bc587773210a82
rmrbest <calesti2980>
2019-03-26 11:15:05 +0100

    refactor(payments collection): refactor in each payment controller to instantiate a payment provider and send to playWithQueue

--- src/apps/web/controllers/PowerBallPaymentController.php
+++ src/apps/web/controllers/PowerBallPaymentController.php
@@ -16,7 +16,9 @@ use EuroMillions\web\vo\CardHolderName;
 use EuroMillions\web\vo\CardNumber;
 use EuroMillions\web\vo\CreditCard;
 use EuroMillions\web\vo\CVV;
+use EuroMillions\web\vo\enum\PaymentSelectorType;
 use EuroMillions\web\vo\ExpiryDate;
+use EuroMillions\web\vo\PaymentCountry;
 use Money\Currency;
 use Money\Money;
 
@@ -87,8 +89,14 @@ class PowerBallPaymentController extends CartController
                     try {
                         $card = new CreditCard(new CardHolderName($card_holder_name), new CardNumber($card_number), new ExpiryDate($expiry_date_month . '/' . $expiry_date_year), new CVV($cvv));
                         $amount = new Money((int)str_replace('.', '', $funds_value), new Currency('EUR'));
-                         $result = $playService->playWithQueue($user_id, $amount, $card, $payWallet, $isWallet, 'EuroMillions', $this->paymentsCollection);
-                          //  $powerball_service->play($user_id, $amount, $card, $payWallet, $isWallet,$result->getValues()[0]->getLottery()->getName());
+                        $result = $playService->playWithQueue(
+                            $user_id,
+                            $amount,
+                            $card,
+                            $payWallet,
+                            $isWallet,
+                            'PowerBall',
+                            $this->paymentProviderService->createCollectionFromTypeAndCountry(PaymentSelectorType::CREDIT_CARD_METHOD,new PaymentCountry(['ES'])));
                         return $this->playResult($result,'powerball');
                     } catch (\Exception $e) {
                         $errors[] = $e->getMessage();
