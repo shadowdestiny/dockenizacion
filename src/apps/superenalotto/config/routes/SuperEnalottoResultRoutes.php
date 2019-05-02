d598b334ce69cea7fa002e1970f3f6ac3bd296ed
Luis Raúl Marin <lrm.programador@gmail.com>
2019-05-02 15:44:45 -0400

    EUR-2145 fix routes 2138

--- src/apps/superenalotto/config/routes/SuperEnalottoResultRoutes.php
+++ src/apps/superenalotto/config/routes/SuperEnalottoResultRoutes.php
@@ -15,42 +15,42 @@ class SuperEnalottoResultRoutes extends RouterGroup
     {
 
         //LAST RESULTS
-        $this->add("/{lottery:(super-enalotto)+}/results", array(
+        $this->add("/{lottery:(superenalotto)+}/results", array(
             "module" => "superenalotto",
             'controller' => 'super-enalotto-numbers',
             'action' => 'index',
             'language' => 'en'
         ));
 
-        $this->add("/{language:(es|it|nl|ru)+}/{lottery:(super-enalotto)+}/{result:(resultados|estrazioni|uitslagen|результаты)+}", array(
+        $this->add("/{language:(es|it|nl|ru)+}/{lottery:(superenalotto)+}/{result:(resultados|estrazioni|uitslagen|результаты)+}", array(
             "module" => "superenalotto",
             'controller' => 'super-enalotto-numbers',
             'action' => 'index',
         ));
 
         //PAST DATES
-        $this->add("/{lottery:(super-enalotto)+}/results/draw-history-page/{date:([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])+)}", array(
+        $this->add("/{lottery:(superenalotto)+}/results/draw-history-page/{date:([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])+)}", array(
             "module" => "superenalotto",
             'controller' => 'super-enalotto-numbers',
             'action' => 'pastResult',
             'language' => 'en'
         ));
 
-        $this->add("/{language:(es|it|nl|ru)+}/{lottery:(super-enalotto)+}/{result:(resultados|estrazioni|uitslagen|результаты)+}/{lastdraw:(sorteos-anteriores|archivio|trekking-geschiedenis|история-розыгрышей)+}/{date:([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])+)}", array(
+        $this->add("/{language:(es|it|nl|ru)+}/{lottery:(superenalotto)+}/{result:(resultados|estrazioni|uitslagen|результаты)+}/{lastdraw:(sorteos-anteriores|archivio|trekking-geschiedenis|история-розыгрышей)+}/{date:([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])+)}", array(
             "module" => "superenalotto",
             'controller' => 'super-enalotto-numbers',
             'action' => 'pastResult',
         ));
 
         //DRAW HISTORY
-        $this->add("/{lottery:(super-enalotto)+}/results/draw-history-page", array(
+        $this->add("/{lottery:(superenalotto)+}/results/draw-history-page", array(
             "module" => "superenalotto",
             'controller' => 'super-enalotto-numbers',
             'action' => 'pastList',
             'language' => 'en'
         ));
 
-        $this->add("/{language:(es|it|nl|ru)+}/{lottery:(super-enalotto)+}/{result:(resultados|estrazioni|uitslagen|результаты)+}/{lastdraw:(sorteos-anteriores|archivio|trekking-geschiedenis|история-розыгрышей)+}", array(
+        $this->add("/{language:(es|it|nl|ru)+}/{lottery:(superenalotto)+}/{result:(resultados|estrazioni|uitslagen|результаты)+}/{lastdraw:(sorteos-anteriores|archivio|trekking-geschiedenis|история-розыгрышей)+}", array(
             "module" => "superenalotto",
             'controller' => 'super-enalotto-numbers',
             'action' => 'pastList',
