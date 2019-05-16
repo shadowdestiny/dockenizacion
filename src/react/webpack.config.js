9a2cc819d7219d20a24bab67a2ef09d24fa07e8f
Luis Raúl Marin <lrm.programador@gmail.com>
2019-05-15 20:55:36 -0400

    clean webpack

--- src/react/webpack.config.js
+++ src/react/webpack.config.js
@@ -68,24 +68,6 @@ let react_module = {
                 exclude: /(node_modules|bower_components)/,
                 use: {
                     loader: 'babel-loader',
-                    /*options: {
-                        presets: [
-                            '@babel/preset-env',
-                            '@babel/preset-react',
-                            {
-                                plugins: [
-                                    "@babel/plugin-proposal-class-properties",
-                                    ["react-css-modules", {
-                                        "filetypes": {
-                                            ".scss": {
-                                                "syntax": "postcss-scss"
-                                            }
-                                        }
-                                    }]
-                                ]
-                            }
-                        ]
-                    }*/
                 }
             },
             /*{
